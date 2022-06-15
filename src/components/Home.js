c758233518efd989a2ba7324e5c278519f850b37
mohamedraffat <mohamed.raffat7@gmail.com>
2022-06-15 10:34:47 +0200

    PizzaJoint-React

---
+++ src/components/Home.js
@@ -0,0 +1,53 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { motion } from "framer-motion";
+import Loader from './Loader';
+const containerVariants = {
+  hidden: {
+    opacity: 0,
+  },
+  visible: {
+    opacity: 1,
+    transition: {
+      delay: 1.5,
+      duration: 1.5,
+    },
+  },
+  exit: {
+    x: "-100vw",
+    transition: { ease: "easeInOut" },
+  },
+};
+const btnVariants = {
+  hover: {
+    scale: 1.1,
+    boxShadow: "0px 0px 8px rgb(200,255,255)",
+    textShadow: "0px 0px 8px rgb(200,255,255)",
+    transition: {
+      duration: 0.3,
+      yoyo: Infinity,
+    },
+  },
+};
+
+const Home = () => {
+  return (
+    <motion.div
+      className="home container"
+      variants={containerVariants}
+      initial="hidden"
+      animate="visible"
+      exit="exit"
+    >
+      <h2>Welcome to Pizza Joint</h2>
+      <Link to="/base">
+        <motion.button variants={btnVariants} whileHover="hover">
+          Create Your Pizza
+        </motion.button>
+      </Link>
+      <Loader/>
+    </motion.div>
+  );
+};
+
+export default Home;
